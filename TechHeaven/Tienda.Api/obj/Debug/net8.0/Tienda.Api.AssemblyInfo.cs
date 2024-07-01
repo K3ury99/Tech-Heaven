b359c59f5a8bf611552b2827700b8f2f2e94a283
K3ury <keury2004@hotmail.com>
2024-07-01 19:09:52 -0400

    Proyect: Updated

--- TechHeaven/Tienda.Api/obj/Debug/net8.0/Tienda.Api.AssemblyInfo.cs
+++ TechHeaven/Tienda.Api/obj/Debug/net8.0/Tienda.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tienda.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1389eb91bfca00be4cf23f424cba09bbf21ffd57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcf671e6b30b79433874da18ed362d9bb1c3eccd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tienda.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tienda.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
